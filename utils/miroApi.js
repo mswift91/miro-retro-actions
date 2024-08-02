27a080a53e581f232745f1cf1e2170df504c1d6a
mswift91 <mswift91@gmail.com>
2024-08-02 10:40:42 +0100

    Implement fetchAllItems function

--- utils/miroApi.js
+++ utils/miroApi.js
@@ -7,3 +7,25 @@ const api = axios.create({
     'Authorization': `Bearer ${miro.token}`
   }
 });
+
+async function fetchAllItems(boardId) {
+    let items = [];
+    let nextUrl = `/boards/${boardId}/items`;
+    
+    while (nextUrl) {
+      try {
+        const response = await api.get(nextUrl);
+        items = items.concat(response.data.data);
+        nextUrl = response.data.links.next ? response.data.links.next.replace('https://api.miro.com/v2', '') : null;
+      } catch (error) {
+        console.error('Error fetching data from Miro API:', error.response ? error.response.data : error.message);
+        throw error;
+      }
+    }
+    
+    return items;
+  }
+  
+  module.exports = {
+    fetchAllItems,
+  };
