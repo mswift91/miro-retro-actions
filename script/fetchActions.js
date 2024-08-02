32ecaad9edd06d7e51bd60fc6169aeca380cde7c
mswift91 <mswift91@gmail.com>
2024-08-02 11:25:00 +0100

    Implement sendToSlack function

--- script/fetchActions.js
+++ script/fetchActions.js
@@ -2,3 +2,21 @@ const { slack } = require('../config/config');
 const { WebClient } = require('@slack/web-api');
 
 const slackClient = new WebClient(slack.token);
+
+async function sendToSlack(message) {
+    try {
+      console.log('Sending message to Slack channel:', slack.channel);
+      await slackClient.chat.postMessage({
+        channel: slack.channel,
+        text: message,
+      });
+      console.log('Message sent to Slack');
+    } catch (error) {
+      console.error('Error sending message to Slack:', error);
+    }
+  }
+  
+  module.exports = {
+    sendToSlack,
+  };
+  
